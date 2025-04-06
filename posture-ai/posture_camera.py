ac60a4cc976a089e8aa2117eeaf6d140084276b1
DevrajBijpuria <dbijpuria@gmail.com>
2025-04-06 19:56:29 +0530

    Update posture_camera.py

--- posture-ai/posture_camera.py
+++ posture-ai/posture_camera.py
@@ -1,1 +1,25 @@
+import cv2
+import mediapipe as mp
 
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(frame_rgb)
+
+    if results.pose_landmarks:
+        mp.solutions.drawing_utils.draw_landmarks(
+            frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    cv2.imshow("Posture Monitor", frame)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
