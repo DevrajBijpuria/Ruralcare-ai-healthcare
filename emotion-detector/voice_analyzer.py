fdfeef29f9b0fe5040f3ae3716a0c0a849ffd449
DevrajBijpuria <dbijpuria@gmail.com>
2025-04-07 00:20:53 +0530

    Delete emotion-detector directory

--- emotion-detector/voice_analyzer.py
+++
@@ -1,7 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_pitch(audio_path):
-    y, sr = librosa.load(audio_path)
-    pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
-    return np.mean(pitches[pitches > 0])
